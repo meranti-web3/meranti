dc68fbccd99c76ae32079cf5b569a7ccf9283577
Olivier Scherrer <pode.fr@gmail.com>
2022-07-28 18:56:18 +0200

    Remove GA4 for GRPD reasons

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,16 +3,5 @@ module.exports = {
     title: `Meranti Web3`,
     siteUrl: `https://meranti.fr`,
   },
-  plugins: [
-    {
-      resolve: "gatsby-plugin-google-gtag",
-      options: {
-        trackingIds: ["G-GKZE7CF2KD"],
-      },
-      pluginConfig: {
-        head: true,
-        respectDNT: true,
-      },
-    },
-  ],
+  plugins: [],
 };
